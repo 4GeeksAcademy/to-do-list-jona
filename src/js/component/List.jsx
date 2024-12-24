d92d01fd1b7830c893f202e95972aac9cc2c59d6
JonathanM1206 <jonathanmoesses@gmail.com>
2024-12-24 01:52:51 +0000

    To do List Jona

--- src/js/component/List.jsx
+++ src/js/component/List.jsx
@@ -16,7 +16,7 @@ const List = () => {
     } 
     const Clear=()=>{ 
         setItems([]) 
-        setInputValue('')
+      
     }
 
 
@@ -26,14 +26,14 @@ const List = () => {
         <h1>Su Lista De Pendientes</h1> 
       <input name="Input" type="text" value={inputValue} onChange={(e)=>setInputValue(e.target.value)}/>  
       <button onClick={additems}>Anote</button>  
-      <button onClick={Clear}>Borrar Todo</button>
+     
 
       <h3>Su Lista: </h3> 
    
     <ul> 
         { 
             items.map((item,index)=>{ 
-                return <li key={index}>{item}</li>
+                return <li key={index}>{item}  <button onClick={Clear}>X</button></li>
             })
         }
       </ul>
