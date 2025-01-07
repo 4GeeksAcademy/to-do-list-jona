c22722facb75f6dfb0fce123d0369f89dfc5f48d
JonathanM1206 <jonathanmoesses@gmail.com>
2025-01-07 23:53:10 +0000

    Lista Recordatorio

--- src/js/component/List.jsx
+++ src/js/component/List.jsx
@@ -1,45 +1,75 @@
-import React,{useState} from 'react'
+import React,{useState} from 'react'  
+import'../component/index.css' 
+
+
+
 
 const List = () => {  
     const [inputValue, setInputValue]=useState('');  
-    const [items, setItems]=useState([]); 
+    const [items, setItems]=useState([]);  
+  
 
-    const additems=()=>{ 
+    
+
+   const additems=()=>{ 
         if(inputValue !==''){ 
 
             setItems([...items,inputValue]); 
              setInputValue('') ;
              return
-        }  
-        alert("Esl espacio esta vacio!!")
+        } 
+        alert("El espacio esta vacio!!")
 
-    } 
-    const Clear=()=>{  
-      setItems([]) 
-   
-      
+    }  
+     
+    const Presionar=(event)=>{ 
+      if(event.keyCode===13){ 
+     additems()
+      }
     }
+    const inputStyles = {
+      width: "230px",
+      height: "28px",
+      fontSize: "16px",
+  };  
+  const handlermouse=(e)=>{  
+    e.target.style.backgroundColor='black';
+
+
+
+  } 
+  const handlermouse2=(e)=>{ 
+    e.target.style.background='white';
+  }
+ 
+
 
 
   return (
-    <div>
-      
-        <h1>Su Lista De Pendientes</h1> 
-      <input name="Input" type="text" value={inputValue} onChange={(e)=>setInputValue(e.target.value)}/>  
-      <button onClick={additems}>Anote</button>  
+    <div >
+      <div className='Cuerpo'>
+      <h1>Su Lista De Pendientes</h1> 
+      <input name="Input"   onKeyDown={Presionar} placeholder='"No hay tareas, añadir tareas"' type="text" value={inputValue} onChange={(e)=>setInputValue(e.target.value)} style={inputStyles}/>  
+      {/* <button onClick={additems}>Anote</button>   */}
      
-
+      <div>{items.length}tasks</div>
       <h3>Su Lista: </h3> 
    
-    <ul> 
+    <ul className='list-group'> 
         { 
             items.map((item,index)=>{ 
-                return <li key={index}>{item}  <button onClick={()=>setItems(items.filter((_,currentIndex)=>index != currentIndex))}>X</button></li>
+                return <li className='list-group-item d-flex li-none justify-content-between align-items-center' key={index} >{item}  
+                           {/* <span className='boton2'onClick={()=>setItems(prev=>prev.filter((_,currentIndex)=>index != currentIndex))} onMouseOver={handlermouse} onMouseLeave={handlermouse2}>X</span>  */}
+                           <span className='btn btn-danger'onClick={()=>setItems(prev=>prev.filter((_,currentIndex)=>index != currentIndex))}>X</span> 
+                        </li>
             })
         }
       </ul>
-   
+ 
       
+
+      </div>
+        
     </div>
   )
 }
